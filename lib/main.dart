c6d94c145b11a99f9ab5b8011f5a835dd0419563
a.22308051281088 <a.22308051281088@cbtis128.edu.mx>
2025-03-03 18:50:36 +0000

    le hice mas chica la matricula

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,8 @@ class MiImagen extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Joselyn Perez-Mat: 22308051281088'),
-          backgroundColor: const Color.fromARGB(255, 250, 227, 99), // Color pastel para el AppBar
+          title: Text('Joselyn Perez-22308051281088'),
+          backgroundColor: const Color.fromARGB(255, 248, 244, 114), // Color pastel para el AppBar
         ),
         body: Center(
           child: Row(
@@ -21,7 +21,7 @@ class MiImagen extends StatelessWidget {
                 width: 100,
                 height: 100,
                 decoration: BoxDecoration(
-                  Color.fromARGB(255, 254, 205, 87),
+                  color: const Color.fromARGB(255, 251, 254, 87),
                   borderRadius: BorderRadius.circular(10),
                   border: Border.all(color: Colors.black, width: 1),
                 ),
